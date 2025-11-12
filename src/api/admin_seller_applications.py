07f15f0c5262950c20b3188d2c473ca0b9fd1072
exl <levzavadskiy999@gmail.com>
2025-11-12 20:27:48 +0300

    add second rout for admin - review seller application

--- src/api/admin_seller_applications.py
+++ src/api/admin_seller_applications.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from datetime import datetime
+
+from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 
 from src.api.dependencies import DBDep
 from src.models.seller_applications import SellerApplicationsOrm
-from src.schemas.enums import SellerApplicationsStatus
-from src.schemas.seller_applications import SellerApplicationOut, SellerApplicationCreate
+from src.models.shops import ShopsOrm
+from src.schemas.enums import SellerApplicationsStatus, ShopStatus
+from src.schemas.seller_applications import SellerApplicationOut, SellerApplicationCreate, SellerApplicationsReview
 
 router = APIRouter(prefix="/admin/seller-applications", tags=["Админка для заявок"])
 
@@ -18,6 +21,66 @@ async def list_seller_applications(db: DBDep, status: SellerApplicationsStatus |
     result = (await db.session.execute(stmt)).scalars().all()
     return result
 
+@router.post("/review/{application_id}", response_model=SellerApplicationOut)
+async def review_seller_application(
+        application_id: int,
+        payload: SellerApplicationsReview,
+        db: DBDep
+):
+    stmt = select(SellerApplicationsOrm).filter(
+        SellerApplicationsOrm.id ==application_id
+    )
+    res = await db.session.execute(stmt)
+    application: SellerApplicationsOrm | None = res.scalar_one_or_none()
+
+    if application is None:
+        raise HTTPException(status_code=404, detail="Заявка не найдена")
+
+    if application.status != SellerApplicationsStatus.pending:
+        raise HTTPException(status_code=400, detail="Это заявка была уже рассмотрена")
+
+    if not payload.approve:
+        if not payload.rejection_reason:
+            raise HTTPException(status_code=400, detail="Требуется указать причину отказа")
+
+        application.status = SellerApplicationsStatus.rejected
+        application.rejection_reason = payload.rejection_reason
+        application.reviewed_at = datetime.utcnow()
+
+        await db.commit()
+        await db.session.refresh(application)
+        return application
+
+    stmt_shop = select(ShopsOrm).filter(
+        ShopsOrm.owner_id == application.user_id
+    ).limit(1)
+    existing_shop = (await db.session.execute(stmt_shop)).scalar_one_or_none()
+    if existing_shop:
+        raise HTTPException(status_code=400, detail="У пользователя уже существует магазин")
+
+    shop = ShopsOrm(
+        owner_id=application.user_id,
+        application_id=application_id,
+        name=application.shop_name,
+        avatar_url=application.avatar_url,
+        description=application.description,
+        city=application.city,
+        inn=application.inn,
+        business_type=application.business_type,
+        shop_type=application.shop_type,
+        status=ShopStatus.active,
+    )
+
+    db.session.add(shop)
+
+    application.status = SellerApplicationsStatus.approved
+    application.reviewed_at = datetime.utcnow()
+    application.rejection_reason = None
+
+    await db.commit()
+    await db.session.refresh(application)
+
+    return application
 
 
 
